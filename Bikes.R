f2a5cfeb179eed8d16880bdd946937f4acb7fc55
sarasneath <saraksneath@gmail.com>
2025-09-04 10:08:54 -0400

    I'm adding a practice plot using SST data

---
+++ Bikes.R
@@ -0,0 +1,20 @@
+#Build a simple graph
+
+#load packages
+library(ggplot2)
+library(EVR628tools)
+
+#load data
+data("data_heatwaves")
+
+#build a plot
+p <- ggplot(data = data_heatwaves,
+       mapping = aes(x = year, y = temp_mean,
+                     group = paste(fishery, eu_rnpa))) +
+  geom_line()
+
+#export my plot
+ggsave(plot = p,
+       filename = "results/img/mean_temp.png")
+
+
